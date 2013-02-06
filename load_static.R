258c88a2b1b3537abeddbd2c386828819928fa07
Nicholas Knoblauch <nwknoblauch@gmail.com>
2013-02-06 13:39:01 -0500

    First attempt at a data loader script

---
+++ load_static.R
@@ -0,0 +1,42 @@
+#!/home/nwk2/R-2.15.2/bin/Rscript
+##Script to read in static data and save it to a Rdata file 
+#nifty, as it reads from the command line
+library(MatrixEQTL)
+
+
+#Usage  <Use_MatrixEQTL_FileReader(T|F)> <SNP_File> <Expression_File > <SNP_Annotation_File> <Expression_Annotation_File> <Output_Rdata_Path>
+#Returns a list in that order
+
+
+#Helper Functions
+load.data.matrix <- function(filepath){
+  #Reads in data matrices
+  rawdat <- read.csv.sql(filepath,sep="\t",header=T,eol="\n")
+  rownames(rawdat)<- rawdat[,1]
+  rawdat <- rawdat[,-1]
+  rawdat <- data.matrix(rawdat)
+  colnames(rawdat)<- gsub("_","-",colnames(rawdat))
+  return(rawdat)
+}
+load.anno <- function(filepath){
+  #Reads in Annotations
+  read.csv.sql(filepath,sep="\t",header=T,eol="\n")
+}
+
+args <- commandArgs(trailingOnly=TRUE)
+
+
+if(args[1]=="T"){
+  snps <- SlicedData$new()
+  snps$fileDelimiter <- "\t"
+  snps$fileOmitCharacters <- "-1"
+  snps$fileSkipRows <- 1
+  snps$fileSkipColumns <- 1
+  snps$LoadFile(args[2])
+  
+  gene <- SlicedData$new(load.data.matrix(args[3])) 
+}else{
+  snps <- SlicedData$new(load.data.matrix(args[2]))
+  gene <- SlicedData$new(load.data.matrix(args[3]))
+}
+  save(list(snp=snps,exp=gene,snp.loc=load.anno(args[4]),exp.loc=load.anno(args[5])),file=args[6])
