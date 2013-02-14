150a68f08a88c51a3f38170db3204ee2677419cb
Nicholas Knoblauch <nwknoblauch@gmail.com>
2013-02-14 11:02:55 -0500

    specify number of cores

--- glm_predict.R
+++ glm_predict.R
@@ -27,7 +27,7 @@ registry.name <- paste("glm_reg_")
 
 
 glm_predict <- function(snp.exploc,train.index,test.index,eqtl.file){
-  registerDoParallel()
+  registerDoParallel(cores=5)
   load(snp.exploc)
   snp.exp$snps <- as.matrix(snp.exp$snps)
   snp.exp$gene <- as.matrix(snp.exp$gene)
