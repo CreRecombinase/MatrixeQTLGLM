ca437b502cfddd3bc3d6775ebd3b653ab2d1d31a
Nicholas Knoblauch <nwknoblauch@gmail.com>
2013-03-06 13:51:42 -0500

    should be ready to produce prediction matrix

--- glm_predict.R
+++ glm_predict.R
@@ -1,76 +1,72 @@
 #Script for creating prediction matrix for eQTLs from glmnet
 library(plyr)
-library(sqldf)
 library(glmnet)
 library(BatchExperiments)
-library(doParallel)
 library(reshape2)
 
-dbfile <- "D:/gene_snp.db"
+dbfile <- "/scratch/nwk2/mEQTL_ERpnc/glmEQTL/unimputed_brca/gene_snp.db"
+dbo <- "/scratch/nwk2/mEQTL_ERpnc/glmEQTL/unimputed_brca/predict.db"
 db <- dbConnect(drv=dbDriver("SQLite"),dbname=dbfile,loadable.extensions=T)
-agenes <- dbGetQuery(db,"select distinct Gene,Kfold from eqtls")
+all.iters <- dbGetQuery(db,"select distinct Gene,Kfold from eqtls")
 
-
-
-
-
-all.iters <- lapply(chunk(1:nrow(agenes),n.chunks=2000),function(x)agenes[x,])
-
-t.iters <- all.iters[[1]]
-colnames(t.iters)<- c("gn","kf")
-gn <- t.iters[1,1]
-kf <- as.integer(t.iters[1,2])
-glm_predict <- function(db,train.index,test.index){
-  registerDoParallel(cores=6)
-
-  system.time(a.resultes <- mlply(t.iters[1:50,],.fun=function(kf,gn,dbfile){
+all.iters <- lapply(chunk(1:nrow(all.iters),n.chunks=200),function(x)all.iters[x,])
+glm_predict <- function(t.iters,dbout,dbfile){
+  m_ply(.data=t.iters,.fun=function(Kfold,Gene,dbfile,dbo){
     db <- dbConnect(drv=dbDriver("SQLite"),dbname=dbfile)
-    querytrain <- paste0("select * from Snps where Sample in (select Sample from trainSamples where Kfold = ",kf,") and Snps in (select SNP from eqtls where Gene='",gn,"' and Kfold=",kf,")order by Sample")
+    querytrain <- paste0("select * from Snps where Sample in (select Sample from trainSamples where Kfold = ",Kfold,") and Snps in (select SNP from eqtls where Gene='",Gene,"' and Kfold=",Kfold,")order by Sample")
     snp.train <- dbGetQuery(db,querytrain)
-    snp.train <- acast(data=snp.train,formula=Sample~Snps)
-    querytest <- paste0("select * from Snps where Sample in (select Sample from testSamples where Kfold = ",kf,") and Snps in (select SNP from eqtls where Gene='",gn,"' and Kfold=",kf,") order by Sample")
+    snp.train <- acast(data=snp.train,formula=Sample~Snps,value.var="Value")
+    querytest <- paste0("select * from Snps where Sample in (select Sample from testSamples where Kfold = ",Kfold,") and Snps in (select SNP from eqtls where Gene='",Gene,"' and Kfold=",Kfold,") order by Sample")
     snp.test <- dbGetQuery(db,querytest)
-    snp.test <- acast(data=snp.test,formula=Sample~Snps)
-    exp.query <- paste0("select Value from gene where Sample in (select Sample from trainSamples where Kfold=",kf,") and Gene='",gn,"' order by Sample")
+    snp.test <- acast(data=snp.test,formula=Sample~Snps,value.var="Value")
+    exp.query <- paste0("select Value from gene where Sample in (select Sample from trainSamples where Kfold=",Kfold,") and Gene='",Gene,"' order by Sample")
     exp.train <- unlist(dbGetQuery(db,exp.query))
-    cv1 <- cv.glmnet(x=snp.train,exp.train,alpha=0.95)
-    tpred <- data.frame(predict(cv1,newx=snp.test,s=cv1$lambda.1se))
-    colnames(tpred)<-gn
+    cv1 <- tryCatch(cv.glmnet(x=snp.train,exp.train,alpha=0.95),error=function(e)e)
+    
+    if(inherits(cv1,"error")){
+      if(sum(sort(tabulate(snp.train),decreasing=T)[-1])>=2){
+        t <- 0
+        while(inherits(cv1,"error")||t<10){
+          cv1 <- tryCatch(cv.glmnet(x=snp.train,exp.train,alpha=0.95),error=function(e)e)
+          t <- t+1
+        }
+        if(t>=10){
+          return(NULL)
+        }
+        tpred <- predict(cv1,newx=snp.test,s=cv1$lambda.1se)
+        npred <- data.frame(Sample=rownames(tpred),Value=tpred[,1])
+        odb <- dbConnect(drv=dbDriver("SQLite"),dbname=dbo)
+        db.success <- tryCatch(dbWriteTable(odb,name="predict",npred,row.names=F),error=function(e)e)
+        dbDisconnect(odb)
+        while(inherits(db.success,"error")){
+          odb <- dbConnect(drv=dbDriver("SQLite"),dbname=dbo)
+          db.success <- tryCatch(dbWriteTable(odb,name="predict",npred,row.names=F),error=function(e)e)
+          dbDisconnect(odb)
+        }
+      }
+    }else{
+      tpred <- predict(cv1,newx=snp.test,s=cv1$lambda.1se)
+      npred <- data.frame(Sample=rownames(tpred),Value=tpred[,1])
+      odb <- dbConnect(drv=dbDriver("SQLite"),dbname=dbo)
+      db.success <- tryCatch(dbWriteTable(odb,name="predict",npred,row.names=F,append=T),error=function(e)e)
+      dbDisconnect(odb)
+      while(inherits(db.success,"error")){
+        odb <- dbConnect(drv=dbDriver("SQLite"),dbname=dbo)
+        db.success <- tryCatch(dbWriteTable(odb,name="predict",npred,row.names=F,append=T),error=function(e)e)
+        dbDisconnect(odb)
+      }
+    }
     dbDisconnect(db)
-    return(tpred)
-  },dbfile,.parallel=T,.paropts=list(.packages=c("RSQLite","glmnet","reshape2")),.inform=F))
-  
-  nbsnps <- dbGetQuery(db,"select * from Snps where Snps in (select SNP from eqtls where Gene='15E1.2' and Kfold=39)")
-  test.cases <-  dbGetQuery(db,"select distinct Sample from testSamples")
-  train.cases <- dbGetQuery(db,"select distinct Sample from trainSamples")
-  
-  
-  
-  
-  
-  
+    
+  },dbfile,dbo,.parallel=F,.paropts=list(.packages=c("RSQLite","glmnet","reshape2"),.inorder=F),.inform=F))
  
-  genpred <- function(s.g){
-    cv1 <- cv.glmnet(x=s.g$snp.train,s.g$exp.train,alpha=0.95)
-    tpred <- predict(cv1,newx=s.g$snp.test,s=cv1$lambda.1se)
-    tpred <- data.frame(t(tpred))
-    tpred$gn <- s.g$gn
-    return(tpred)
-  }
-  
-  predmat <- ldply(test.train.snp.exp,.fun=genpred,.parallel=T,.paropts=list(.packages="glmnet",.multicombine=T,.inorder=F))
-  
-  system.time(tpredmat <- ldply(test.train.snp.exp[1:300],.fun=genpred,.parallel=T,.paropts=list(.packages="glmnet",.multicombine=T,.inorder=F)))
-  Rprof(NULL)
-  summaryRprof("glmprof.out")
-  tpredmat
-  return(predmat)
+    
 }
 
 
-glm.reg <- makeRegistry(registry.name,file.dir=m.dir,packages=c("doParallel","glmnet","sqldf","plyr","MatrixEQTL"))
+glm.reg <- makeRegistry(registry.name,file.dir=m.dir,packages=c("glmnet","plyr","reshape2"))
 
-batchMap(glm.reg,glm_predict,train.index=train.indices,test.index=test.indices,eqtl.file=eqtl.files,more.args=list(snp.exploc=snp.exploc))
+batchMap(glm.reg,fun=glm_predict,t.iters=all.iters,more.args=list(dbout=dbo,dbfile=dbfile))
 
 
 
