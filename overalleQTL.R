e095e9140fddde23efbce10699a4843328b09f15
Nicholas Knoblauch <nwknoblauch@gmail.com>
2013-03-08 09:46:57 -0500

    Runs overall eQTL analysis without covariates

---
+++ overalleQTL.R
@@ -0,0 +1,47 @@
+#Code to run a single overall MatrixEQTL (without covariates)
+#3/8/13
+#NWK
+library(MatrixEQTL)
+
+snp.type <- "imputed"
+cancer.type <- "brca"
+root.dir <- "/scratch/nwk2/mEQTL_ERpnc/imputed_eqtl/"
+out.dir <- paste(root.dir,"output")
+
+setwd(root.dir)
+
+
+annofile <- "rnaseq_brca_anno.Rdata"
+snp.expdata <- "rnaseq_snp_exp.Rdata"
+  
+MEQTL.params <- list(
+  output.file.name.tra=paste(out.dir,snp.type,"_",cancer.type,"_trans",sep=""),
+  output.file.name.cis=paste(out.dir,snp.type,"_",cancer.type,"_cis",sep=""),
+  useModel=modelLINEAR,
+  verbose=T,
+  pvOutputThreshold.tra=1e-8,
+  pvOutputThreshold.cis=1e-8,
+  cisDist=1e6,
+  pvalue.hist=F
+)
+  
+
+load(snp.exploc)
+load(anno.loc)
+with(MEQTL.params,Matrix_eQTL_main(
+      snps=snp.exp$snps,
+      gene=snp.exp$gene,
+      output_file_name=paste(output.file.name.tra,i,".txt",sep=""),
+      output_file_name.cis=paste(output.file.name.cis,i,".txt",sep=""),
+      useModel=useModel,
+      verbose=verbose,
+      pvOutputThreshold=pvOutputThreshold.tra,
+      pvOutputThreshold.cis=pvOutputThreshold.cis,
+      snpspos=annolist$snp.anno,
+      genepos=annolist$exp.anno,
+      cisDist=cisDist,
+      pvalue.hist=pvalue.hist
+    )
+  )
+  
+}
