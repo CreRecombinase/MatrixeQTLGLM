27c0731dda4de9b65798b0505266c6e144948e88
Nicholas Knoblauch <nwknoblauch@gmail.com>
2013-03-08 10:22:02 -0500

    hopefully last fix necessary for overall eQTL analysis

--- overalleQTL.R
+++ overalleQTL.R
@@ -31,8 +31,8 @@ load(annofile)
 with(MEQTL.params,Matrix_eQTL_main(
       snps=snp.exp$snps,
       gene=snp.exp$gene,
-      output_file_name=paste(output.file.name.tra,i,".txt",sep=""),
-      output_file_name.cis=paste(output.file.name.cis,i,".txt",sep=""),
+      output_file_name=paste(output.file.name.tra,".txt",sep=""),
+      output_file_name.cis=paste(output.file.name.cis,".txt",sep=""),
       useModel=useModel,
       verbose=verbose,
       pvOutputThreshold=pvOutputThreshold.tra,
