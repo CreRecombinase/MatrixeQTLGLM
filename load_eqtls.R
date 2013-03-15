8cf5151b015bda3bd7ed282bb1e7c58b23491d79
Nicholas Knoblauch <nwknoblauch@gmail.com>
2013-03-15 10:02:44 -0400

    now loads testing and training data into database as well

--- load_eqtls.R
+++ load_eqtls.R
@@ -3,10 +3,14 @@ library(sqldf)
 library(RSQLite)
 
 args <- list()
-##USAGE <RESULTS_DIR> <DATABSE_FILE>
+##USAGE <RESULTS_DIR> <DATABSE_FILE> <KFOLD> <SAMPLE_NUM> <SAMPLE_FILE>
+
 oargs <- commandArgs(trailingOnly=TRUE)
 args$RESULTS_DIR <- oargs[1]
 args$DATABASE_FILE <- oargs[2]
+args$KFOLD <- oargs[3]
+args$SAMPLE_NUM <- oargs[4]
+args$SAMPLE_FILE <- oargs[5]
 
 
 
@@ -16,69 +20,39 @@ cis.transs <- gsub(".+(cis|trans)[0-9]+.txt","\\1",eqtl.files)
 
 write.db.file <- function(eqtl.file,cis.trans,kfold,dbfile){
   db <- dbConnect(drv=dbDriver("SQLite"),dbname=dbfile)
-  eqtl.file <- read.csv.sql(eqtl.file,sep="\t",header=T,eol="\n")
-    dbWriteTable
+  eqtl <- read.csv.sql(eqtl.file,sep="\t",header=T,eol="\n")
+  eqtl$Kfold<-kfold
+  eqtl$cis.trans <- cis.trans
+  dbWriteTable(db,name="eqtls",eqtl,row.names=F,append=T)
+  dbDisconnect(db)
 }
 
-base.dir <- "/scratch/nwk2/mEQTL_ERpnc/glmEQTL/brca_RNAseq/"
-
-dbfile <- paste0(base.dir,"rnaseq_snp_genes.db")
-
-eqtl.files <- paste0(base.dir,"82-fold/unimputed_brca_RNAseq_trans")
-eqtl.files <- paste0(eqtl.files,1:82,".txt")
-
-exp.file <- paste0(base.dir,"brca_RNAseq_expression.txt")
-snp.file <- paste0(base.dir,"unimputed_brca_RNAseq_snp.txt")
-sample.num <- 819
-kfold <- 82
+mapply(FUN=write.db.file,eqtl.file=eqtl.files,cis.trans=cis.transs,kfold=kfolds,MoreArgs=list(dbfile=args$DATABASE_FILE))
 
 
 
-train.indices <- chunk(rep(1:sample.num,kfold),n.chunks=kfold)
-test.indices <- chunk(1:sample.num,chunk.size=ceiling(sample.num/kfold))
+
+
+
+train.indices <- chunk(rep(1:args$SAMPLE_NUM,args$KFOLD),n.chunks=args$KFOLD)
+test.indices <- chunk(1:args$SAMPLE_NUM,chunk.size=ceiling(args$SAMPLE_NUM/args$KFOLD))
 train.indices <- mapply(FUN=function(x,y)x[-y],train.indices,test.indices,SIMPLIFY=F)
 
 
-
-
-aeqtls <- ldply(eqtl.files,function(x){
-  tqtl <- read.csv.sql(x,header=T,eol="\n",sep="\t")
-  knum <- gsub(".+trans([0-9]+).txt","\\1",x)
-  tqtl$knum <- knum
-  return(tqtl)
-}
-)
-
-db <- dbConnect(drv=dbDriver("SQLite"),dbname=dbfile,loadable.extensions=T)
-colnames(aeqtls) <- c("SNP","Gene","tStat","pValue","FDR","Kfold")
-dbWriteTable(db,"eqtls",aeqtls,row.names=F)
+db <- dbConnect(drv=dbDriver("SQLite"),dbname=args$DATABSE_FILE)
 dbSendQuery(db,"Create index sgk on eqtls(SNP,Gene,Kfold)")
 dbSendQuery(db,"Create index gk on eqtls(Gene,Kfold)")
 
-
-
-fsnp.genes <- split(aeqtls,aeqtls$knum)
-snp.genes <- unlist(lapply(fsnp.genes,function(x)split(x$SNP,x$gene)),recursive=F)
-rm(aeqtls,fsnp.genes)
-
-snp.cases <- scan(snp.file,what="character",nlines=1,sep="\n")
-snp.cases <- strsplit(snp.cases,split="\t")[[1]]
-snp.cases <- snp.cases[-1]
-exp.cases <- scan(exp.file,what="character",nlines=1,sep="\n")
-exp.cases <- strsplit(exp.cases,"\t")[[1]]
-exp.cases <- exp.cases[-1]
-if(any(exp.cases!=snp.cases)){
-  stop("Problem with MatrixEQTL inputs")
-  
-}
+sample.cases <- scan(args$SAMPLE_CASES,what="character",sep="\n",nlines=1)
+sample.cases <- strsplit(sample.cases,"\t")[[1]][-1]
 
 
 test.df <- do.call("rbind",lapply(1:length(test.indices),function(x){
-  data.frame(Sample=exp.cases[test.indices[[x]]],Kfold=x,Index=test.indices[[x]])
+  data.frame(Sample=sample.cases[test.indices[[x]]],Kfold=x,Index=test.indices[[x]])
 }))
 
 train.df <-test.df <- do.call("rbind",lapply(1:length(train.indices),function(x){
-  data.frame(Sample=exp.cases[train.indices[[x]]],Kfold=x,Index=train.indices[[x]])
+  data.frame(Sample=sample.cases[train.indices[[x]]],Kfold=x,Index=train.indices[[x]])
 }))
 
 dbWriteTable(db,name="testSamples",test.df,row.names=F,overwrite=T,append=F)
