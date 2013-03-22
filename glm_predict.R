2cce863aa130ff0ed3a0e1d0546751984d62cbc2
Nicholas Knoblauch <nwknoblauch@gmail.com>
2013-03-22 17:35:20 -0400

    likely performance increase on GLM_Predict

--- glm_predict.R
+++ glm_predict.R
@@ -1,11 +1,12 @@
 #Script for creating prediction matrix for eQTLs from glmnet
 ## library(plyr,quietly=T)
-## library(glmnet,quietly=T)
-## library(BatchExperiments,quietly=T)
-## library(reshape2,quietly=T)
-## library(RSQLite,quietly=T)
+library(glmnet,quietly=T)
+
+library(BatchExperiments,quietly=T)
+library(reshape2,quietly=T)
+library(RSQLite,quietly=T)
 ## library(compiler,quietly=T)
-## library(doParallel,quietly=T)
+library(doParallel,quietly=T)
 
 #usage glm_predict.R <DBFILE> <chunks> <out.dir> <queue> <memory> <time> <threads>
 #makeClusterFunctionsLSF("~/lsf-threaded.tmpl")
@@ -23,35 +24,55 @@
 
 
 
-## db <- dbConnect(drv=dbDriver("SQLite"),dbname=dbfile,loadable.extensions=T)
+db <- dbConnect(drv=dbDriver("SQLite"),dbname=dbfile,loadable.extensions=T)
 
-#system.time(all.iters <- dbGetQuery(db,"select distinct eqtls.gene,Kfold from eqtls,ggenes where eqtls.gene=ggenes.Gene"))
+
+all.iters <- dbGetQuery(db,"select distinct gene from ggenes")[[1]]
+
+
 ## dbGetQuery(db,"pragma journal_mode=TRUNCATE")
 ## dbDisconnect(db)
+all.iters <- chunk(all.iters,n.chunks=200)
+max.kfold <- 44
+
+
+
 
 
   
-#all.iters <- lapply(chunk(1:nrow(all.iters),n.chunks=chunks),function(x)all.iters[x,])
 
+ot.iters <- all.iters[[1]]
 
-glm_predict <- function(t.iters,dbfile,threads){
+glm_predict <- function(ot.iters,dbfile,threads,kfolds){
+  
+  create.set <- function(items){
+    items <-paste0("'",items,"'")
+    paste0("(",Reduce(function(x,y)paste(x,y,sep=","),items),")")
+  }
+  
+  
+  snps <- dbGetQuery(db,paste0("select SNP,gene,Kfold from eqtls where gene in ",create.set(ot.iters)))
+  all.exp <- acast(dbGetQuery(db,paste0("select * from gene where gene in ",create.set(ot.iters))),formula=Sample~Gene,value.var="Value")
+  nnsql <- paste0("select * from snps where Snp in ",create.set(unique(unlist(snpl))))
+  system.time(nnsnps <- dbGetQuery(db,nnsql))
+  trainSamples <- dbGetQuery(db,"select Sample,Kfold from trainSamples")
+  trainSamples <- split(trainSamples$Sample,trainSamples$Kfold)
+  testSamples <- dbGetQuery(db,"select Sample,Kfold from testSamples")
+  testSamples <- split(testSamples$Sample,testSamples$Kfold)
+  asnps <- acast(data=nnsnps,formula=Sample~Snp,value.var="Value")
+  eqtls <- sapply(split(snps,snps$gene),FUN=function(x)split(x$SNP,x$Kfold),simplify=F)
 
+  
+  
+  
+  
+  
   registerDoParallel(cores=threads-1)
   
   glm.engine <- function(gene,Kfold){
-    
-    db <- dbConnect(drv=dbDriver("SQLite"),dbname=dbfile,loadable.extensions=T)
-    dbGetQuery(db,"pragma journal_mode=TRUNCATE")
-    dbGetQuery(db,"pragma busy_timeout=20000")
-    querytrain <- paste("select * from snps where Sample in (select Sample from trainSamples where Kfold = ",Kfold,") and Snp in (select SNP from eqtls where Gene='",gene,"' and Kfold=",Kfold,")order by Sample",sep="")
-    snp.train <- dbGetQuery(db,querytrain)
-    snp.train <- acast(data=snp.train,formula=Sample~Snp,value.var="Value")
-    querytest <- paste("select * from snps where Sample in (select Sample from testSamples where Kfold = ",Kfold,") and Snp in (select SNP from eqtls where Gene='",gene,"' and Kfold=",Kfold,") order by Sample",sep="")
-    snp.test <- dbGetQuery(db,querytest)
-    snp.test <- acast(data=snp.test,formula=Sample~Snp,value.var="Value")
-    exp.query <- paste("select Value from gene where Sample in (select Sample from trainSamples where Kfold=",Kfold,") and Gene='",gene,"' order by Sample",sep="")
-    exp.train <- unlist(dbGetQuery(db,exp.query))
-    dbDisconnect(db)
+    snp.train <- asnps[trainSamples[[Kfold]],eqtls[[gene]][[Kfold]],drop=F]
+    snp.test<- asnps[testSamples[[Kfold]],eqtls[[gene]][[Kfold]],drop=F]
+    exp.train <- all.exp[trainSamples[[Kfold]],gene]
     cv1 <- tryCatch(cv.glmnet(x=snp.train,exp.train,alpha=0.95),error=function(e)e)
     if(inherits(cv1,"error")){
       if(any(apply(snp.train,MARGIN=2,function(x)sum(sort(tabulate(x),decreasing=T)[-1]))>=2)){
@@ -63,23 +84,26 @@ glm_predict <- function(t.iters,dbfile,threads){
           cv1 <- tryCatch(cv.glmnet(x=snp.train,exp.train,alpha=0.95),error=function(e)e)
           t <- t+1
         }
-        if(t>=10){
+        if(t>=3){
           return(NULL)
         }
         tpred <- predict(cv1,newx=snp.test,s=cv1$lambda.1se)
         npred <- data.frame(Sample=rownames(tpred),Value=tpred[,1],Gene=gene)
-        return(npred)
+        return(list(npred,as.matrix(coef(cv1.fit,s=cv1$lamda.1se))))
       }     
     }else{
       tpred <- predict(cv1,newx=snp.test,s=cv1$lambda.1se)
       npred <- data.frame(Sample=rownames(tpred),Value=tpred[,1],Gene=gene)
-      return(npred)
+      cf <- as.matrix(coef(cv1.fit,s=cv1$lamda.1se))
+      
+      return(list(npred,)))
      
     }
     
   }
-  system.time(tt.res <- mdply(.data=t.iters,.fun=glm.engine,.parallel=T,.paropts=list(.multicombine=T,.inorder=F,.verbose=F,.export=c("glm.engine","dbfile"),.packages=c("glmnet","RSQLite","reshape2"))))
-  tt.res <- tt.res[,c("Sample","Value","Gene")]
+
+  t.iters <- expand.grid(gene=ot.iters,Kfold=as.character(1:kfolds),stringsAsFactors=F)
+  system.time(tt.res <- mlply(.data=t.iters,.fun=glm.engine,.parallel=T,.inform=F,.paropts=list(.multicombine=T,.inorder=F,.verbose=F,.export=c("glm.engine","asnps","all.exp","eqtls","trainSamples","testSamples"),.packages=c("glmnet"))))
   return(tt.res)
     
 }
