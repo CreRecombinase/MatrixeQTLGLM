b6d16d46e25d44b2e84fdbe0bd0d04d14afb4f9e
Nicholas Knoblauch <nwknoblauch@gmail.com>
2013-04-16 15:43:38 -0400

    code to create databases

--- create_db.R
+++ create_db.R
@@ -1,33 +1,71 @@
-#Code to generate list of lists to be fed into glm_predict.R
-
-library(MatrixEQTL)
-library(plyr)
-library(BatchExperiments)
-library(doParallel)
-library(sqldf)
+#Code to create expression and snp DBs
 library(RSQLite)
+library(MatrixEQTL)
+library(sqldf)
 library(reshape2)
-if(Sys.info()['sysname']=="Windows"){
-  snp.exploc <- "C:/Users/nknoblau/Documents/R_WS/MatrixeQTLGLM/test/snp.exp.Rdata"
-  
-}else{
-  snp.exploc <- "/scratch/nwk2/mEQTL_ERpnc/glmEQTL/unimputed_brca/snp.exp.Rdata"
-}
+library(fastmatch)
+#USAGE Ctype Trans|Cis
 
-load(snp.exploc)
-snp.exp$snps <- t(as.matrix(snp.exp$snps))
-snp.exp$gene <- t(as.matrix(snp.exp$gene))
-snp.exp$gene <- snp.exp$gene[rownames(snp.exp$snps),]
+oargs <- commandArgs(trailingOnly=T)
+ctype <- oargs[1]
+TC <- oargs[2]
+setwd("/scratch/nwk2/pancan")
+eqtl.file <- paste0("output/unimputed_",ctype,"_",TC,".txt")
+eqtl.dat <- read.csv.sql(eqtl.file,sep="\t",header=T,eol="\n")
 
-db <- dbConnect(drv=dbDriver("SQLite"),"2ngene_snp.db",synchronous=0)
+dat.file <- paste0(ctype,".Rdata")
 
-gene.melt <- melt(snp.exp$gene)
-colnames(gene.melt) <- c("Sample","Gene","Value")
-dbRemoveTable(db,"genes")
-dbSendQuery(db,"CREATE TABLE genes (Sample, Gene Text, Value REAL)")
-dbSendQuery(db,"CREATE INDEX gs ON genes(Gene ASC,Sample ASC)")
+load(dat.file)
 
-gene.melt <- gene.melt[order(gene.melt$Gene,gene.melt$Sample),]
-dbWriteTable(db,"genes",gene.melt,append=T,overwrite=F,row.names=F)
+snps <- as.matrix(snp.exp$snps)
 
-dbDisconnect(db);
+exp <- as.matrix(snp.exp$gene)
+rm(snp.exp)
+gc()
+
+snps <- snps[rownames(snps) %in% eqtl.dat$SNP,]
+exp <- exp[rownames(exp) %in% eqtl.dat$gene,]
+
+asnpnum <- apply(snps,1,function(x)sum(sort(tabulate(x+1),decreasing=T)[-1]))
+agenenum <- apply(exp,1,function(x)sum(x!=0))
+
+eqtl.dat$snpnum <- asnpnum[ fmatch(eqtl.dat$SNP,names(asnpnum))]
+eqtl.dat$genenum <- agenenm[ fmatch(eqtl.dat$gene,names(agenenm))]
+
+write.table(eqtl.dat,file=eqtl.file,sep="\t",col.names=T,row.names=F,quote=F,append=F)
+
+bsnp <- melt(snps)
+colnames(bsnp) <- c("Snp","Sample","Value")
+dbGetQuery(db,"pragma journal_mode=memory")
+dbGetQuery(db,"pragma synchronous=0")
+dbGetQuery(db,"pragma cache_size=250000")
+dbWriteTable(db,"snps",bsnp)
+dbGetQuery(db,"pragma temp_store=1")
+dbGetQuery(db,"pragma temp_store_directory='.'")
+dbGetQuery(db,"create index ss on snps(Snp,Sample")
+
+rm(bsnp)
+gc()
+
+bexp <- melt(exp)
+colnames(exp) <- c("Gene","Sample","Value")
+dbWriteTable(db,"gene",bexp,row.names=F)
+
+dbGetQuery(db,"create index gs on gene(Gene,Sample)")
+
+
+dbDisconnect()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
